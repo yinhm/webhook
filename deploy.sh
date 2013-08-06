5361339f9556d8d75d4ff1f2e577f3c6de92883e
HaiMing.Yin <HaiMing.Yin@gmail.com>
2013-08-06 15:18:13 +0800

    bash specific source.

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@
 cd /srv/www/cgt/src
 /usr/bin/git pull
 /usr/bin/supervisorctl restart cgt
-source /srv/www/envs/cgt/bin/activate
+# source are csh-ism
+. /srv/www/envs/cgt/bin/activate
 python manage.py collectstatic --noinput --settings=cgt.production
